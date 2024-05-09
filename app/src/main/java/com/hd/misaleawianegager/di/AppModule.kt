8541b1d88bd4a2349e843d6e36bdfb813f7e02a0
hailat1998 <htemesgen400@gmail.com>
2024-05-09 09:02:59 +0000

    added module

--- app/src/main/java/com/hd/misaleawianegager/di/AppModule.kt
+++ app/src/main/java/com/hd/misaleawianegager/di/AppModule.kt
@@ -1,6 +1,12 @@
 package com.hd.misaleawianegager.di
 
 import android.text.method.SingleLineTransformationMethod
+import com.hd.misaleawianegager.data.local.AssetsTextServiceImp
+import com.hd.misaleawianegager.data.local.FileServiceImp
+import com.hd.misaleawianegager.data.local.WorkerTextServiceImp
+import com.hd.misaleawianegager.domain.local.AssetsTextService
+import com.hd.misaleawianegager.domain.local.FileService
+import com.hd.misaleawianegager.domain.local.WorkerTextService
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -32,7 +38,17 @@ object AppModule {
     @Provides
     fun bindDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
 
+    @Singleton
+    @Provides
+    fun bindFileService() : FileService = FileServiceImp()
 
+    @Singleton
+    @Provides
+    fun bindAssetsService() : AssetsTextService = AssetsTextServiceImp()
+
+    @Singleton
+    @Provides
+    fun bindWorkerTextService(): WorkerTextService = WorkerTextServiceImp()
 
 
 }
